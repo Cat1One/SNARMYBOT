c71cf3aca2b0d732a627fb44effc82d08256dea4
Glitch (sn-army) <none>
2020-09-18 10:10:40 +0000

    💿🎅 Checkpoint ./server.js:3423186/1 ./watch.json:3423186/6

--- server.js
+++ server.js
@@ -229,4 +229,4 @@ client.on("message", (message) => {
     }
   });
 });
-client.login("NzU1NDMyNjM0NTQ4NDg2MTk2.X2DNdw.alMoI-9i_thhMPEJyHcfvtXnzr");
+client.login("NzU1NDMyNjM0NTQ4NDg2MTk2.X2DNdw.alMoI-9i_thhMPEJyHcfvtXnzr0");
