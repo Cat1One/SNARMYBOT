ee58cadd534838c99b4488534fc37d2bbfa6f197
Glitch (sn-army) <none>
2020-09-19 16:19:38 +0000

    🏗🐔 Checkpoint ./server.js:3423186/4230 ./Nada.txt:3423186/3174

--- server.js
+++ server.js
@@ -19,19 +19,15 @@ app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
 }, 280000);
-
-
-//DESDE AQUI EMPIEZA A ESCRIBIR EL CODIGO PARA SU BOT
-
+//////////////////////DESDE AQUI EMPIEZA EL BOT//////////////////////
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 let prefix = config.prefix;
-//Mensaje de inici
+//////////////////////BIENVENIDA//////////////////////
   client.on("ready", () => {
   console.log("Encendido");
-  client.channels.find(x => x.name === "𝘾𝙊𝙉𝙁𝙄𝙂𝙐𝙍𝘼𝘾𝙄𝙊𝙉").send(":crown: Iniciando sistema :crown:");
-});
+  client.channels.find(x => x.name === "𝘾𝙊𝙉𝙁𝙄𝙂𝙐𝙍𝘼𝘾𝙄𝙊𝙉").send(":crown: Iniciando sistema :crown:");});
 client.on("message", (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   client.on("guildMemberAdd", member => {
@@ -43,11 +39,9 @@ client.on("message", (message) => {
       .setThumbnail(member.user.displayAvatarURL)
       .setColor("RANDOM")
       .setFooter(message.guild.name)
-      canal.send(embed)
-  })
-//Comandos
+      canal.send(embed)})
+//////////////////////REGLAS//////////////////////
   if (message.content.startsWith(prefix + "Reglas")) {
-
     const embed = new Discord.RichEmbed()
       .setTitle("📜 | Reglas del servidor")
       .setColor(0xc556d8)
@@ -60,10 +54,8 @@ client.on("message", (message) => {
       .addField("•","No difundir informacion tanto publica como privada de las personas de dentro como externas a la comunidad")
       .setFooter("SN Army", client.user.avatarURL)
       .setTimestamp()
-    message.channel.send({ embed });
-  }
+    message.channel.send({ embed });}
   if (message.content.startsWith(prefix + "reglas")) {
-
     const embed = new Discord.RichEmbed()
       .setTitle("📜 | Reglas del servidor")
       .setColor(0xc556d8)
@@ -76,82 +68,8 @@ client.on("message", (message) => {
       .addField("•","No difundir informacion tanto publica como privada de las personas de dentro como externas a la comunidad")
       .setFooter("SN Army", client.user.avatarURL)
       .setTimestamp()
-    message.channel.send({ embed });
-  }
-  if (message.content.startsWith(prefix + "Roles")) {
-    const embed = new Discord.RichEmbed()
-      .setTitle("🧾| ELEGI TU RANGO")
-      .setColor(0xc556d8)
-      .setDescription("• Tenes que reaccionar en el emoji que se indica para cada uno")
-      .setFooter("SN Army", client.user.avatarURL)
-      .setTimestamp()
-    message.channel.send({ embed });
-  }
-   if (message.content.startsWith(prefix + "Generos")) {
-    const embed = new Discord.RichEmbed()
-      .setTitle("🧖| ELEGI TU GENERO")
-      .setColor(0xc556d8)
-      .setDescription("• Reacciona con :woman: para recibir el rol de mujer o con :man: para recibir el rol de hombre")
-      .setFooter("SN Army", client.user.avatarURL)
-      .setTimestamp()
-    message.channel.send({ embed });
-  }
-  if (message.content.startsWith(prefix + "Paises")) {
-
-    const embed = new Discord.RichEmbed()
-      .setTitle(":earth_americas:| ELEGI TU PAIS")
-      .setColor(0xc556d8)
-      .setDescription("• Reacciona con la bandera de tu pais para recibir el rol.")
-      .setFooter("SN Army", client.user.avatarURL)
-      .setTimestamp()
-    message.channel.send({ embed });
-  }
-  if (message.content.startsWith(prefix + "twitch")) {
-    const embed = new Discord.RichEmbed()
-      .setTitle("<:TwitchLogo:755632620368298085> | TWITCH")
-      .setColor(0xc556d8)
-      .setDescription("• Reacciona con el emoji <:TwitchLogo:755632620368298085> para recibir notificaciones cuando los streamers del servidor prendan.")
-      .setFooter("SN Army", client.user.avatarURL)
-      .setTimestamp()
-    message.channel.send({ embed });
-  }
-  if (message.content.startsWith(prefix + "economia")) {
-    const embed = new Discord.RichEmbed()
-      .setTitle(":dollar: | CASINO")
-      .setColor(0xc556d8)
-      .setDescription("• Reacciona con el emoji :dollar: para ver las salas del ***Casino***.")
-      .setFooter("SN Army", client.user.avatarURL)
-      .setTimestamp()
-    message.channel.send({ embed });
-  }
-  if (message.content.startsWith(prefix + "CsGo")) {
-    const embed = new Discord.RichEmbed()
-      .setTitle("<:Cs:754138621669277746> | CsGo")
-      .setColor(0xc556d8)
-      .setDescription("• Reacciona con el emoji <:Cs:754138621669277746> para ver las salas del ***Cs:Go***.")
-      .setFooter("SN Army", client.user.avatarURL)
-      .setTimestamp()
-    message.channel.send({ embed });
-  }
-  if (message.content.startsWith(prefix + "Fortnite")) {
-    const embed = new Discord.RichEmbed()
-      .setTitle("<:Fortnite:755640673482440745> | Fortnite")
-      .setColor(0xc556d8)
-      .setDescription("• Reacciona con el emoji <:Fortnite:755640673482440745> para ver las salas del ***Fortnite***.")
-      .setFooter("SN Army", client.user.avatarURL)
-      .setTimestamp()
-    message.channel.send({ embed });
-  }
-  if (message.content.startsWith(prefix + "Lol")) {
-    const embed = new Discord.RichEmbed()
-      .setTitle("<:LoL:754137357015318538> | League Of Legeds")
-      .setColor(0xc556d8)
-      .setDescription("• Reacciona con el emoji <:LoL:754137357015318538> para ver las salas del ***LoL***.")
-      .setFooter("SN Army", client.user.avatarURL)
-      .setTimestamp()
-    message.channel.send({ embed });
-  }
-  //Server
+    message.channel.send({ embed });}
+//////////////////////SERVER//////////////////////
   var server = message.guild;
   if (message.content.startsWith(prefix + "Server")) {
     const embed = new Discord.RichEmbed()
@@ -164,8 +82,8 @@ client.on("message", (message) => {
       .addField('Miembros', server.memberCount, true)
       .addField('Roles', server.roles.size, true)
       .setColor(0x66b3ff)
-    message.channel.send({ embed });
-  } 
+    message.channel.send({ embed });}
+//////////////////////KISS//////////////////////
   let member = message.mentions.members.first();
   if (message.content.startsWith(prefix + "kiss")) {
   if (message.mentions.users.size < 1) return message.reply('Debe mencionar a un miembro.').catch(console.error);
@@ -174,9 +92,8 @@ client.on("message", (message) => {
       .setAuthor(server.name, server.iconURL)  
       .setTitle(`<@${member.id}>`, "ah besado a", `<@${member.id}>`)
       .setColor(0x66b3ff)
-    message.channel.send({ embed });
-  };
-  //Server
+    message.channel.send({ embed });};
+//////////////////////AVATAR//////////////////////
   if (message.content.startsWith(prefix + "avatar")) {
   if (message.mentions.users.size < 1) return message.reply('Debe mencionar a un miembro.').catch(console.error);
     const embed = new Discord.RichEmbed() 
@@ -184,10 +101,8 @@ client.on("message", (message) => {
       .setAuthor(server.name, server.iconURL)
       .addField("Aca tenes el avatar", `<@${member.id}>`)
       .setColor(0x66b3ff)
-    message.channel.send({ embed });
-  }
-  
-//8Ball
+    message.channel.send({ embed });}
+//////////////////////8Ball//////////////////////
 const command = args.shift().toLowerCase();
 let texto = args.join(" ");
 if(command === '8ball'){
@@ -195,7 +110,7 @@ if(command === '8ball'){
     if (!texto) return message.reply(`Escriba una pregunta.`);
     message.channel.send(`${message.author}`+ ' mi respuesta es a su pregunta es: `'+ rpts[Math.floor(Math.random() * rpts.length)]+'`');
   
-//msg consola
+//////////////////////MSG_CONSOLA//////////////////////
   client.on('ready', () => { });
   console.log("Actualizado");
 //Estado de twitch
@@ -204,8 +119,7 @@ if(command === '8ball'){
     game: {
       name: "SN Army | -Ayuda",
       type: "STREAMING",
-      url: "https://discord.gg/3HTdCDw"
-      }
+      url: "https://discord.gg/3HTdCDw"}
     });
   }
 });
