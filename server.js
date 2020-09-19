80b074e255d0df978e547c54fd6dccc3de0d17e2
Glitch (sn-army) <none>
2020-09-19 14:10:54 +0000

    📫🍇 Checkpoint ./server.js:3423186/6043

--- server.js
+++ server.js
@@ -194,23 +194,9 @@ if(command === '8ball'){
     var rpts = ["Sí", "No", "¿Por qué?", "Por favor", "Tal vez", "No sé", "Definitivamente?", " ¡Claro! "," Sí "," No "," Por supuesto! "," Por supuesto que no "];
     if (!texto) return message.reply(`Escriba una pregunta.`);
     message.channel.send(`${message.author}`+ ' mi respuesta es a su pregunta es: `'+ rpts[Math.floor(Math.random() * rpts.length)]+'`');
+
   
-//Comando ayuda
-if(command === 'ayuda'){
-    message.channel.send('**'+message.author.username+'**, Revisa tus mensajes privados.');
-    message.author.send('**COMANDOS DE MYBOT**\n```\n'+
-                        '-> '+prefix+'ping           :: Comprueba la latencia del bot y de tus mensajes.\n'+
-                        '-> '+prefix+'avatar <@user> :: Muestra el avatar de un usuario.\n'+
-                        '-> '+prefix+'decir          :: Hace que el bot diga un mensaje.\n'+
-                        '-> '+prefix+'user <@user>   :: Muestra información sobre un usuario mencioando.\n'+
-                        '-> '+prefix+'server         :: Muestra información de un servidor determinado.\n'+
-                        '-> '+prefix+'8ball          :: El bot respondera a tus preguntas.\n'+
-                        '-> '+prefix+'ban <@user>    :: Banear a un usuario del servidor incluye razon.\n'+
-                        '-> '+prefix+'kick <@user>   :: Patear a un usuario del servidor incluye razon.\n'+
-                        '-> '+prefix+'hola           :: Retorna un saludo como mensaje.\n```\n\n'+
-                        '**MyBOT - Server guía y de soporte Únete :**\nhttps://discord.gg/VxwER6t');
-}
-  
+
 //msg consola
   client.on('ready', () => { });
   console.log("Actualizado");
