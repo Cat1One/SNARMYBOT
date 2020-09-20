dff7c80f0a7d3132a5e6383a131c24604f61414e
Glitch (sn-army) <none>
2020-09-20 07:27:20 +0000

    💾☃️ Checkpoint ./server.js:3423186/1879 ./watch.json:3423186/4

--- server.js
+++ server.js
@@ -88,11 +88,10 @@ client.on("message", (message) => {
   if (message.content.startsWith(prefix + "kiss")) {
   if (message.mentions.users.size < 1) return message.reply('Debe mencionar a un miembro.').catch(console.error);
   const embed = new Discord.RichEmbed() 
-    .setImage("https://cdn.discordapp.com/attachments/399448944889036801/664504993352515594/5738ac03-e8c8-481e-a5f9-3fa14761635f.gif")
-    .setAuthor(server.name, server.iconURL)  
-    .addField(`<@${member.id}>`, "ah besado a")
-    .setColor(0x66b3ff)
-    .setTimestamp()
+      .setImage("https://cdn.discordapp.com/attachments/399448944889036801/664504993352515594/5738ac03-e8c8-481e-a5f9-3fa14761635f.gif")
+      .setAuthor(server.name, server.iconURL)  
+      .addField(`<@${member.id}>`, "ah besado a")
+      .setColor(0x66b3ff)
     message.channel.send({ embed });}
 //////////////////////AVATAR//////////////////////
   if (message.content.startsWith(prefix + "avatar")) {
@@ -121,8 +120,7 @@ if(command === '8ball'){
     game: {
       name: "SN Army | -Ayuda",
       type: "STREAMING",
-      url: "https://discord.gg/3HTdCDw"
-    }
+      url: "https://discord.gg/3HTdCDw"}
     });
   }
 });
