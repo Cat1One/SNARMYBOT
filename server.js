56e690a8e48fc679c4eb7330ffe31b29022d1c80
Glitch (sn-army) <none>
2020-09-20 09:45:40 +0000

    👭🐥 Checkpoint ./server.js:3423186/19989

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ const client = new Discord.Client();
 const config = require("./config.json");
 let prefix = config.prefix;
 const token = "NzU1NDMyNjM0NTQ4NDg2MTk2.X2DNdw.alMoI-9i_thhMPEJyHcfvtXnzr0";
-const PREFIX = "$";
 ////////////////////////////// MENSAJE DE INICIO //////////////////////////////
   client.on("ready", () => {
   console.log("Encendido");
@@ -114,40 +113,6 @@ client.on("message", (message) => {
     .setTimestamp()
     message.channel.send({ embed });
   }
-  
-function doKissAction() {
-    var rand = [
-        'https://media2.giphy.com/media/G3va31oEEnIkM/giphy.gif',
-        'https://media1.tenor.com/images/f5167c56b1cca2814f9eca99c4f4fab8/tenor.gif?itemid=6155657',
-        'https://media.tenor.com/images/fbb2b4d5c673ffcf8ec35e4652084c2a/tenor.gif',
-        'https://media.giphy.com/media/ZRSGWtBJG4Tza/giphy.gif',
-        'https://media.giphy.com/media/oHZPerDaubltu/giphy.gif',
-        'https://acegif.com/wp-content/uploads/anime-kiss-m.gif',
-        'https://media.giphy.com/media/bm2O3nXTcKJeU/giphy.gif',
-        'https://media.giphy.com/media/nyGFcsP0kAobm/giphy.gif',
-        'https://media0.giphy.com/media/KH1CTZtw1iP3W/source.gif'
-    ];
- 
-    return rand[Math.floor(Math.random() * rand.length)];
-}
- 
-bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
- 
-    switch (args[0]) {
-                   case 'kiss':
-                const personTagged = message.mentions.members.first();
- 
-                if(!args[1]) {
-                    message.channel.send('You are missing arguments!')
-                }else{
-                    message.channel.send('`' + message.author.username + '`' + ' has kissed ' + personTagged.displayName + ' ' + doKissAction())
-                }
- 
-            break;
- 
-   }
-})
 ////////////////////////////// 8BALL //////////////////////////////
 const command = args.shift().toLowerCase();
 let texto = args.join(" ");
